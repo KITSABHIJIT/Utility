417d54cd2a4d840c28d6658d7e90ea9b2454a979
Abhijit Roy <abhijit.roy@staples.com>
2017-03-21 14:36:06 -0400

    Added Image Utility code

---
+++ ImageUtil/src/main/java/com/imageUtil/code/CommonConstants.java
@@ -0,0 +1,7 @@
+package com.imageUtil.code;
+
+public class CommonConstants {
+	
+	public static final String FILE_PATH = System.getProperty("user.home")+System.getProperty("file.separator")+"ImageUtil";
+	
+}
