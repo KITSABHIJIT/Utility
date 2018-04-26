0278adad14396d6fa467d4d7ccd569763007ca91
Abhijit Roy <abhijit.roy@staples.com>
2018-04-26 14:57:16 -0400

    Added KafkaLogFeeder project

---
+++ KafkaLogFeeder/src/com/staples/kafka/log/util/StringUtilTest.java
@@ -0,0 +1,74 @@
+package com.staples.kafka.log.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringUtilTest {
+
+	@Test
+	public void testIsBlankOrEmptyString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPadZeros() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsBlankOrEmptyObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testValidateString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStrToInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStrToLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIntToStr() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTrim() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvertIntoNumeric() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeriveInvoiceNo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCustomFormat() {
+		fail("Not yet implemented");
+	}
+
+}
