e17129f60472721278c5f2694f6f11e360e56d6d
Roy, Abhijit <Abhijit.Roy@Staples.com>
2019-01-31 14:33:05 -0500

    Image extractor modified

--- ImageTextExtractor/src/com/test/code/Driver.java
+++ ImageTextExtractor/src/com/test/code/Driver.java
@@ -3,6 +3,7 @@ package com.test.code;
 public class Driver {
 	public static void main(String[] args) {  
         // TODO Auto-generated method stub  
-        System.out.println(ImageCracker.crackImage("image/bill3.gif"));  
+        System.out.println(ImageCracker.crackImage("/Users/royab001/Google Drive/Utility Bills/Ria/Receipts/sample.png"));  
+        //System.out.println(ImageCracker.crackImage("image/Capture.JPG"));
    }  	
 }
