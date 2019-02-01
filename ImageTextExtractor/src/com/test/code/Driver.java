c0cc831c0c9a0e44d4b5b3d96ad7597924ca19e9
Roy, Abhijit <Abhijit.Roy@Staples.com>
2019-02-01 11:12:53 -0500

    Modified code

--- ImageTextExtractor/src/com/test/code/Driver.java
+++ ImageTextExtractor/src/com/test/code/Driver.java
@@ -3,7 +3,7 @@ package com.test.code;
 public class Driver {
 	public static void main(String[] args) {  
         // TODO Auto-generated method stub  
-        System.out.println(ImageCracker.crackImage("/Users/royab001/Google Drive/Utility Bills/Ria/Receipts/sample.png"));  
-        //System.out.println(ImageCracker.crackImage("image/Capture.JPG"));
+        System.out.println(ImageCracker.crackImage("/Users/royab001/Google Drive/Utility Bills/Ria/ImageReceipts/US545561268.png"));  
+        //System.out.println(ImageCracker.getTextFromPDF("/Users/royab001/Google Drive/Utility Bills/Ria/ImageReceipts/US545548668.pdf"));
    }  	
 }
--- ImageTextExtractor/src/com/test/code/ImageCracker.java
+++ ImageTextExtractor/src/com/test/code/ImageCracker.java
@@ -11,11 +11,31 @@ public class ImageCracker {
 		File imageFile = new File(filePath);  
 		ITesseract instance = new Tesseract();   
 		try {  
+			instance.setPageSegMode(3); 
 			String result = instance.doOCR(imageFile);  
 			return result;  
 		} catch (TesseractException e) {  
 			System.err.println(e.getMessage());  
 			return "Error while reading image";  
 		}  
-	}  
+	}
+
+	public static String getTextFromPDF(String filePath) {
+		String result=null;
+		File image = new File(filePath);
+		// recognizeTextBlocks(image.toPath());
+
+		Tesseract tessInst = new Tesseract();
+		tessInst.setDatapath("/tessdata-master/tessdata");
+		try
+		{
+			result = tessInst.doOCR(image);
+			System.out.println(result);
+		}
+		catch (TesseractException e)
+		{
+			System.err.println(e.getMessage());
+		}
+		return result;
+	}
 }
