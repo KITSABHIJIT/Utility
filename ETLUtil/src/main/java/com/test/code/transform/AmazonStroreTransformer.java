62ed099a732c1769216ebd0671cd5d7d7e928b3b
abhijit <abhijit@abhijits-air.fios-router.home>
2021-11-21 17:16:36 -0500

    Updated transformation logic

--- ETLUtil/src/main/java/com/test/code/transform/AmazonStroreTransformer.java
+++ ETLUtil/src/main/java/com/test/code/transform/AmazonStroreTransformer.java
@@ -33,14 +33,23 @@ public class AmazonStroreTransformer {
 			csvReader = new CSVReader(new FileReader(PropertiesUtil.getProperty("StoreCardFile")),COMMA_DELIMITER,QUOTE_CHAR,1);
 			while((expenseDetails = csvReader.readNext())!=null)
 			{
-				if(!PAYMENT_DONE.equalsIgnoreCase(expenseDetails[4].trim()) 
-						&& !PAYMENT_DONE1.equalsIgnoreCase(expenseDetails[4].trim())
-						&& !PAYMENT_DONE2.equalsIgnoreCase(expenseDetails[4].trim())) {
+				if(!expenseDetails[4].trim().toUpperCase().contains(PAYMENT_DONE) 
+						&& !expenseDetails[4].trim().toUpperCase().contains(PAYMENT_DONE1)
+						&& !expenseDetails[4].trim().toUpperCase().contains(PAYMENT_DONE2)) {
 					Expense exp = new Expense();
 					exp.setModeOfPayment(MODE_OF_PAYMENT);
 					exp.setTransactionDate(DateUtil.getSQLData(DateUtil.getSomeDate(expenseDetails[0].trim(), "MM/dd/yyyy")));
-					exp.setMerchant(expenseDetails[4].trim().toUpperCase());
-					exp.setExpensePlace(expenseDetails[5].trim().toUpperCase());
+
+					if(expenseDetails.length>5) {
+						exp.setMerchant(expenseDetails[4].trim().toUpperCase());
+						exp.setExpensePlace(expenseDetails[5].trim().toUpperCase());
+					}else if(expenseDetails[4].contains("       ")){
+						exp.setMerchant(expenseDetails[4].split("       ")[0].trim().toUpperCase());
+						exp.setExpensePlace(expenseDetails[4].substring(expenseDetails[4].indexOf("       ")).trim().toUpperCase());	
+					}else {
+						exp.setMerchant(expenseDetails[4].trim().toUpperCase());
+					}
+
 					exp.setAmount(-1*StringUtil.getDouble(expenseDetails[3].trim()));
 					exp.setReferenceNo(StringUtil.trim(expenseDetails[2]));
 					if(expenseList.contains(exp)) {
